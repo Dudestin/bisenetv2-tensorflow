96ac3468aa7d1b6be60b6dae375245755f4e01b8
Dudestin <splashkakaponear@gmail.com>
2020-12-05 11:00:42 +0900

    Update train_bisenetv2_segcomp.py

--- tools/segcomp/train_bisenetv2_segcomp.py
+++ tools/segcomp/train_bisenetv2_segcomp.py
@@ -8,12 +8,14 @@
 """
 Train bisenetv2 on cityscapes dataset
 """
-from trainner.cityscapes import cityscapes_bisenetv2_single_gpu_trainner as single_gpu_trainner
+import sys
+sys.path.insert(1, '/content/bisenetv2-tensorflow-colab')
+from trainner.segcomp import segcomp_bisenetv2_single_gpu_trainner as single_gpu_trainner
 from local_utils.log_util import init_logger
 from local_utils.config_utils import parse_config_utils
 
 LOG = init_logger.get_logger('train_bisenetv2_segcomp')
-CFG = parse_config_utils.cityscapes_cfg_v2
+CFG = parse_config_utils.segcomp_cfg
 
 
 def train_model():
